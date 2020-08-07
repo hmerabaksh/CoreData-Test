9781e620950612b4c203fdc20643ea46f984d7b8
Hussain Farbotko-Merabaksh <hmerabaksh@gmail.com>
2020-08-07 17:39:54 -0400

    Initial Commit

---
+++ CoreData-Test/CoreData_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_TestApp.swift
+//  CoreData-Test
+//
+//  Created by Hussain Farbotko-Merabaksh on 2020-08-07.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreData-TestTests/CoreData_TestTests.swift
@@ -0,0 +1,33 @@
+//
+//  CoreData_TestTests.swift
+//  CoreData-TestTests
+//
+//  Created by Hussain Farbotko-Merabaksh on 2020-08-07.
+//
+
+import XCTest
+@testable import CoreData_Test
+
+class CoreData_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
